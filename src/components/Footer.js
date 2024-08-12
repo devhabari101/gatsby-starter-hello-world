208b2201a926d537eaac275249cc363d20ff06b1
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-08-12 10:12:14 +0300

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p>&copy; 2024 Travel Agency. All Rights Reserved.</p>
+            </div>
+        </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
