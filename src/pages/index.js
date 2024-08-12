b55b32402b9cfda4c0435a78362681471b41f9a6
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-08-12 12:23:18 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import React from "react";
 import Header from "../components/Header";
 import MainContent from "../components/MainContent";
 import Footer from "../components/Footer";
-import "/styles/global.css"; // Import global CSS here
 
 const IndexPage = () => (
   <>
