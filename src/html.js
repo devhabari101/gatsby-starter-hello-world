6de7a2b9f3c65ef86948547c97f448fa5384a887
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-08-12 11:41:20 +0300

    Create html.js

---
+++ src/html.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {props.headComponents}
+        <link rel="stylesheet" href="./src/styles/global.css"; />
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.preBodyHtml }}
+        />
+        {props.bodyComponents}
+        <script src="./src/js/script.js"></script>
+      </body>
+    </html>
+  );
+}
